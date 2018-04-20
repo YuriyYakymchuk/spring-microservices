1dfda5f774aab9bd8932313985e7d560b96237f9
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2018-04-20 07:38:29 +0300

    Lab-1 Task-2. Added Eureka client dependency and enabled eure discovery

--- guest/src/main/java/com/ogasimov/labs/springcloud/microservices/guest/GuestApp.java
+++ guest/src/main/java/com/ogasimov/labs/springcloud/microservices/guest/GuestApp.java
@@ -2,8 +2,10 @@ package com.ogasimov.labs.springcloud.microservices.guest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GuestApp {
     public static void main(String[] args) {
         SpringApplication.run(GuestApp.class, args);
