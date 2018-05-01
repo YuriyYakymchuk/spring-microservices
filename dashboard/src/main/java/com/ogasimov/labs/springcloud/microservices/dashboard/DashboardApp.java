af9a78fd17d56d790f4eb4b38ad18a8e66f4a73e
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2018-05-01 23:44:23 +0300

    Implemented Lab-2, Task-1 (Dashboard)

--- dashboard/src/main/java/com/ogasimov/labs/springcloud/microservices/dashboard/DashboardApp.java
+++ dashboard/src/main/java/com/ogasimov/labs/springcloud/microservices/dashboard/DashboardApp.java
@@ -2,8 +2,12 @@ package com.ogasimov.labs.springcloud.microservices.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
 public class DashboardApp {
     public static void main(String[] args) {
         SpringApplication.run(DashboardApp.class, args);
