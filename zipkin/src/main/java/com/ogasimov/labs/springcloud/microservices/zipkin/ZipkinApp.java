b463e13a37add9ea2c58ce5ef12b4abaae9ff9e0
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2018-05-02 17:47:21 +0300

    Lab-3, Task-3. Added Zipkin

--- zipkin/src/main/java/com/ogasimov/labs/springcloud/microservices/zipkin/ZipkinApp.java
+++ zipkin/src/main/java/com/ogasimov/labs/springcloud/microservices/zipkin/ZipkinApp.java
@@ -2,8 +2,12 @@ package com.ogasimov.labs.springcloud.microservices.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApp {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApp.class, args);
