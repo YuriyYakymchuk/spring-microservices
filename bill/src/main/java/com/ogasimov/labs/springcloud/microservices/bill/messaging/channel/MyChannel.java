0d798326460aa6bd521de0895d501388c3d5fabb
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2018-05-31 14:10:53 +0300

    Lab-7 Added Auth service

--- bill/src/main/java/com/ogasimov/labs/springcloud/microservices/bill/messaging/channel/MyChannel.java
+++ bill/src/main/java/com/ogasimov/labs/springcloud/microservices/bill/messaging/channel/MyChannel.java
@@ -1,8 +1,6 @@
 package com.ogasimov.labs.springcloud.microservices.bill.messaging.channel;
 
 import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
 public interface MyChannel {
