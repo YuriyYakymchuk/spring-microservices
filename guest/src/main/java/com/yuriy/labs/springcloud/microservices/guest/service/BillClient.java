7f3ceb045780fbf731305a1667a593b0853a590e
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2019-02-26 18:02:40 +0200

    Changes after version downgrade

--- guest/src/main/java/com/yuriy/labs/springcloud/microservices/guest/service/BillClient.java
+++ guest/src/main/java/com/yuriy/labs/springcloud/microservices/guest/service/BillClient.java
@@ -1,6 +1,6 @@
 package com.yuriy.labs.springcloud.microservices.guest.service;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
